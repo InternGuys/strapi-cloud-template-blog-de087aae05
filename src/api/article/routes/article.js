d8a221df386842b271846213d8b072386202fc77
Aman Tulsyan <amantulsyan35@gmail.com>
2025-11-14 13:44:26 +0530

    service-change

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -4,11 +4,36 @@
  * article router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
 module.exports = {
   routes: [
-    ...createCoreRouter('api::article.article').routes,
+    {
+      method: 'GET',
+      path: '/articles',
+      handler: 'article.find',
+      config: {
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/articles/:id',
+      handler: 'article.findOne',
+    },
+    {
+      method: 'POST',
+      path: '/articles',
+      handler: 'article.create',
+    },
+    {
+      method: 'PUT',
+      path: '/articles/:id',
+      handler: 'article.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/articles/:id',
+      handler: 'article.delete',
+    },
     {
       method: 'GET',
       path: '/articles/:id/related',
