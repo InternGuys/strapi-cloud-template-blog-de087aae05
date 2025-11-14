1dde86c30ca0aaa6ffa9e0fb9f99c258ef32739a
Aman Tulsyan <amantulsyan35@gmail.com>
2025-11-14 13:33:43 +0530

    add-back

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -6,4 +6,13 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = {
+  routes: [
+    ...createCoreRouter('api::article.article').routes,
+    {
+      method: 'GET',
+      path: '/articles/:id/related',
+      handler: 'article.findRelated'
+    }
+  ]
+};
